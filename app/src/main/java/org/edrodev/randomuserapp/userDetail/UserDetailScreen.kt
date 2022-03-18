5cf4f64802d599c742e519d6fa6149d1f6dda8d7
Pedro A. Ruiz Cuesta <63908711+PedroARuiz@users.noreply.github.com>
2022-03-18 18:46:11 +0100

    Navigation skeleton

---
+++ app/src/main/java/org/edrodev/randomuserapp/userDetail/UserDetailScreen.kt
@@ -0,0 +1,9 @@
+package org.edrodev.randomuserapp.userDetail
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun UserDetailScreen() {
+    Text(text = "UserDetail")
+}
