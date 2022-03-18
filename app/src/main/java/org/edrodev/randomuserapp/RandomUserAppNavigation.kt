f859dc72907a80e3083ed79eab0c5c5f84b5f9db
Pedro Ruiz <pedro518ruiz@gmail.com>
2022-03-18 18:44:59 +0100

    Navigation skeleton

---
+++ app/src/main/java/org/edrodev/randomuserapp/RandomUserAppNavigation.kt
@@ -0,0 +1,26 @@
+package org.edrodev.randomuserapp
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import org.edrodev.randomuserapp.userDetail.UserDetailScreen
+import org.edrodev.randomuserapp.userList.UserListScreen
+
+@Composable
+fun RandomUserAppNavigation() {
+
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "users") {
+
+        composable("users") {
+            UserListScreen()
+        }
+
+        composable("users/{userId}") {
+            UserDetailScreen()
+        }
+    }
+
+}
---
+++ app/src/main/java/org/edrodev/randomuserapp/userList/UserListScreen.kt
@@ -0,0 +1,9 @@
+package org.edrodev.randomuserapp.userList
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun UserListScreen() {
+    Text(text = "Welcome to RandomUserApp!")
+}
