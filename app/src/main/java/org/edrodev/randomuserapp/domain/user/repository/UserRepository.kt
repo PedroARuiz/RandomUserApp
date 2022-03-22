859ae45c6ca6c6cb679fb981fc611642e3496ce8
Pedro A. Ruiz Cuesta <63908711+PedroARuiz@users.noreply.github.com>
2022-03-22 18:39:06 +0100

    Error handling

--- app/src/main/java/org/edrodev/randomuserapp/domain/user/repository/UserRepository.kt
+++ app/src/main/java/org/edrodev/randomuserapp/domain/user/repository/UserRepository.kt
@@ -1,11 +1,12 @@
 package org.edrodev.randomuserapp.domain.user.repository
 
+import arrow.core.Either
 import kotlinx.coroutines.flow.Flow
 import org.edrodev.randomuserapp.domain.user.model.User
 
 interface UserRepository {
 
-    suspend fun fetchUsers(count: Int): List<User>
+    suspend fun fetchUsers(count: Int): Either<Throwable, List<User>>
 
     fun findUsers(): Flow<List<User>>
 
--- app/src/main/java/org/edrodev/randomuserapp/domain/user/useCase/FetchUsersUseCase.kt
+++ app/src/main/java/org/edrodev/randomuserapp/domain/user/useCase/FetchUsersUseCase.kt
@@ -1,5 +1,6 @@
 package org.edrodev.randomuserapp.domain.user.useCase
 
+import arrow.core.Either
 import org.edrodev.randomuserapp.domain.user.model.User
 import org.edrodev.randomuserapp.domain.user.repository.UserRepository
 
@@ -8,5 +9,6 @@ private const val DEFAULT_COUNT_USER_FETCH = 10
 class FetchUsersUseCase(
     private val userRepository: UserRepository,
 ) {
-    suspend operator fun invoke(): List<User> = userRepository.fetchUsers(DEFAULT_COUNT_USER_FETCH)
+    suspend operator fun invoke(): Either<Throwable, List<User>> =
+        userRepository.fetchUsers(DEFAULT_COUNT_USER_FETCH)
 }
